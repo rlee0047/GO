487a68cbeac05c7b03562c1100e9fa6753e8ef69
Ryan Lee <rlee0047@gmail.com>
2019-10-02 18:09:35 -0400

    New Commit

---
+++ Validator/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/go-playground/validator.v9"
+)
+
+type uSer struct {
+	ID        string `validate:"omitempty,uuid"`
+	Firstname string `validate:"omitempty"`
+	Lastname  string `validate:"omitempty"`
+	Username  string `validate:"required,email"`
+	Password  string `validate:"required,gte=10"`
+	Type      string `validate:"isdefault"`
+}
+
+func main() {
+	user := uSer{
+		Firstname: "Ryan",
+		Lastname:  "Lee",
+		Username:  "test@gmail.com",
+		Password:  "1234567890",
+	}
+	validate := validator.New()
+	err := validate.Struct(user)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Welcome,", user.Firstname, user.Lastname)
+	}
+}
